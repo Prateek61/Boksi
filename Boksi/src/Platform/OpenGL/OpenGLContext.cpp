46fd7024ff319023de91732fe87afd46d36f4c07
kiyotone <71599237+kiyotone@users.noreply.github.com>
2024-07-22 02:05:45 +0545

    feat: GPU Version Setup Done

--- Boksi/src/Platform/OpenGL/OpenGLContext.cpp
+++ Boksi/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,9 +19,13 @@ namespace Boksi
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
         BK_CORE_ASSERT(status, "Failed to initialize Glad!");
 
-        BK_CORE_INFO("OpenGL Version: {0}");
-        // BK_CORE_INFO("GLSL Version: {0}", glGetString(GL_SHADING_LANGUAGE_VERSION));
-        // BK_CORE_INFO("OpenGL Vendor: {0}", glGetString(GL_VENDOR));
+
+        std::string version(reinterpret_cast<const char *>(glGetString(GL_VERSION)));
+        BK_CORE_INFO("OpenGL Version: {0}", version);
+        std::string vendor(reinterpret_cast<const char *>(glGetString(GL_VENDOR)));
+        BK_CORE_INFO("OpenGL Vendor: {0}", vendor);
+        std::string renderer(reinterpret_cast<const char *>(glGetString(GL_RENDERER)));
+        BK_CORE_INFO("OpenGL Renderer: {0}", renderer);
     }
 
     void OpenGLContext::SwapBuffers()
